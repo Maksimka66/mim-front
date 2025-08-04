2cda76eb04ae32091e4b644258d08cb9ac70bb80
Maksimka66 <maxfese@gmail.com>
2025-08-04 17:09:24 +0300

    Create wrapperButton.tsx

---
+++ app/components/wrapperButton.tsx
@@ -0,0 +1,11 @@
+import { Button, View } from 'react-native'
+
+const wrapperButton = ({ images, callback, className }) => {
+	return (
+		<View>
+			<Button />
+		</View>
+	)
+}
+
+export default wrapperButton
